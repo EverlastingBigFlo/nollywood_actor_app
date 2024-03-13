5b1de99bc0764291f1c7385b0c762721daee7602
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-13 11:59:42 +0000

    13 Mar 2024, 04:59

--- lib/components/animation/animation.dart
+++ lib/components/animation/animation.dart
@@ -1,19 +1,69 @@
+// import 'package:flutter/material.dart';
+
+// class SlideAnimationExample extends StatefulWidget {
+//   @override
+//   _SlideAnimationExampleState createState() => _SlideAnimationExampleState();
+// }
+
+// class _SlideAnimationExampleState extends State<SlideAnimationExample> with SingleTickerProviderStateMixin {
+//   late AnimationController _controller;
+//   late Animation<Offset> _animation;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = AnimationController(duration: Duration(seconds: 1), vsync: this);
+//     _animation = Tween<Offset>(begin: Offset(-1.0, 0.0), end: Offset.zero).animate(_controller);
+//     _controller.forward();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SlideTransition(
+//       position: _animation,
+//       child: Container(
+//         width: 100,
+//         height: 100,
+//         color: Colors.blue,
+//       ),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-class SlideAnimationExample extends StatefulWidget {
+class SlideAnimation extends StatefulWidget {
+  final Widget child;
+  final Offset beginOffset;
+  final Offset endOffset;
+  final Duration duration;
+
+  SlideAnimation({
+    required this.child,
+    required this.beginOffset,
+    required this.endOffset,
+    required this.duration,
+  });
+
   @override
-  _SlideAnimationExampleState createState() => _SlideAnimationExampleState();
+  _SlideAnimationState createState() => _SlideAnimationState();
 }
 
-class _SlideAnimationExampleState extends State<SlideAnimationExample> with SingleTickerProviderStateMixin {
+class _SlideAnimationState extends State<SlideAnimation> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<Offset> _animation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(duration: Duration(seconds: 1), vsync: this);
-    _animation = Tween<Offset>(begin: Offset(-1.0, 0.0), end: Offset.zero).animate(_controller);
+    _controller = AnimationController(duration: widget.duration, vsync: this);
+    _animation = Tween<Offset>(begin: widget.beginOffset, end: widget.endOffset).animate(_controller);
     _controller.forward();
   }
 
@@ -21,11 +71,7 @@ class _SlideAnimationExampleState extends State<SlideAnimationExample> with Sing
   Widget build(BuildContext context) {
     return SlideTransition(
       position: _animation,
-      child: Container(
-        width: 100,
-        height: 100,
-        color: Colors.blue,
-      ),
+      child: widget.child,
     );
   }
 
