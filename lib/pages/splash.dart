b2ed884d6b7fb9f31a5a7af696739564d8694961
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-13 11:42:01 +0000

    13 Mar 2024, 04:42

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Splash extends StatefulWidget {
-  const Splash({super.key});
+  const Splash({Key? key}) : super(key: key);
 
   @override
   State<Splash> createState() => _SplashState();
@@ -13,17 +12,24 @@ class _SplashState extends State<Splash> {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.black,
-      body: Padding(
-        padding: EdgeInsets.all(0),
-        child: Row(
-          children: [
-            Center(
-              child: Column(
-                children: [Image(image: AssetImage('lib/assets/Group 38.png'))],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: Image(
+                image: AssetImage('lib/assets/Group 38.png'),
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: Icon(Icons.phone_bluetooth_speaker),
+            ),
+          ),
+        ],
       ),
     );
   }
