9b7e6fc2cd999ed479ab81d5bd426a377f14136e
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-13 10:21:26 +0000

    13 Mar 2024, 03:21

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -10,6 +10,9 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      
+    );
   }
 }
