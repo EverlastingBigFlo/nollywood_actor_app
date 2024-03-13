eb36df85ea8a115ad62898b43744facc48126644
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-13 14:46:39 +0000

    13 Mar 2024, 07:46

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -16,6 +16,13 @@ class _OnboardingState extends State<Onboarding> {
     'lib/assets/adesua.png',
     'lib/assets/toyin.png',
   ];
+  final List<String> mySecondActors = [
+    'lib/assets/funke.png',
+    'lib/assets/Ransey.png',
+    'lib/assets/Tobi.png',
+    'lib/assets/adesua.png',
+    'lib/assets/toyin.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +47,7 @@ class _OnboardingState extends State<Onboarding> {
                 // Actors section
                 Positioned(
                   top: 250,
-                  right: -25,
+                  right: -45,
                   child: Transform(
                     transform: Matrix4.skew(-3, 3),
                     child: Padding(
@@ -49,6 +56,34 @@ class _OnboardingState extends State<Onboarding> {
                         children: [
                           Row(
                             children: myFirstActors.map((path) {
+                              return Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image(
+                                    image: AssetImage(path),
+                                  ),
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                Positioned(
+                  top: 400,
+                  right: -45,
+                  child: Transform(
+                    transform: Matrix4.skew(-3, 3),
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: mySecondActors.map((path) {
                               return Padding(
                                 padding: const EdgeInsets.all(12),
                                 child: ClipRRect(
