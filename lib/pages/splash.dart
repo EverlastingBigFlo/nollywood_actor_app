7314305b2c171245f1f98d10c6bb85bf77257f6d
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-13 12:32:57 +0000

    13 Mar 2024, 05:32

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../components/animation/animation.dart';
-
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
 
@@ -9,55 +7,29 @@ class Splash extends StatefulWidget {
   State<Splash> createState() => _SplashState();
 }
 
-class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Offset> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller =
-        AnimationController(duration: Duration(seconds: 1), vsync: this);
-    _animation = Tween<Offset>(begin: Offset(0.1, 0.0), end: Offset.zero)
-        .animate(_controller);
-    _controller.forward();
-  }
-
+class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SlideAnimation(
-            beginOffset: const Offset(0.0, -1.5),
-            endOffset: Offset.zero,
-            duration: const Duration(seconds: 2),
-            position: _animation,
-            child: const Expanded(
-              child: Center(
-                child: Image(
-                  image: AssetImage('lib/assets/Group 38.png'),
-                ),
+          Expanded(
+            child: Center(
+              child: Image(
+                image: AssetImage('lib/assets/Group 38.png'),
               ),
             ),
           ),
-          const Align(
+          Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
-              padding: EdgeInsets.only(bottom: 20),
-              child: Image(image: AssetImage('lib/assets/loader.png')),
-            ),
+                padding: EdgeInsets.only(bottom: 20),
+                child: Image(image: AssetImage('lib/assets/loader.png'))),
           ),
         ],
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 }
