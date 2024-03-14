7cfcf8afaadc99b3c09783881c6e363cd3bd502f
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-14 14:59:00 +0000

    14 Mar 2024, 07:59

---
+++ lib/pages/util/button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final icon;
+  final String number;
+  const MyButton({super.key, this.icon, required this.number});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: const EdgeInsets.symmetric(vertical: 15.0),
+    child: Column(
+      children: [
+        icon,SizedBox(height: 25),
+        Text(number),
+      ],
+    ),
+    );
+  }
+}
