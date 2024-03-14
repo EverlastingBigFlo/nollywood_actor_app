2f37460cf421842df645661f2108c5f28b486863
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-14 03:22:30 +0000

    13 Mar 2024, 20:22

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -100,6 +100,7 @@ class _OnboardingState extends State<Onboarding> {
                     ),
                   ),
                 ),
+              
               ],
             ),
           ),
