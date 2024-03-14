142babd5c70ae5fa870a0707c30c0e3fff5517c5
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-14 11:40:13 +0000

    14 Mar 2024, 04:40

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -31,205 +31,198 @@ class _OnboardingState extends State<Onboarding> {
       body: Stack(
         children: [
           // image sections
-          Center(
-            child: Stack(
-              children: [
-                // Nollywood logo
-                const Positioned(
-                  top: 70,
-                  left: 30,
+          Stack(
+            children: [
+              // Nollywood logo
+              const Positioned(
+                top: 70,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 30),
                   child: Image(
                     image: AssetImage('lib/assets/Group 39.png'),
                   ),
                 ),
+              ),
 
-                // Actors section
-                Positioned(
-                  top: 200,
-                  right: -45,
-                  child: Transform(
-                    transform: Matrix4.skew(-3, 3),
-                    child: Padding(
-                      padding: const EdgeInsets.all(0),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: myFirstActors.map((path) {
-                              return Padding(
-                                padding: const EdgeInsets.all(12),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Image(
-                                    image: AssetImage(path),
-                                  ),
+              // Actors section
+              Positioned(
+                top: 200,
+                right: -45,
+                child: Transform(
+                  transform: Matrix4.skew(-3, 3),
+                  child: Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: myFirstActors.map((path) {
+                            return Padding(
+                              padding: const EdgeInsets.all(12),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image(
+                                  image: AssetImage(path),
                                 ),
-                              );
-                            }).toList(),
-                          ),
-                        ],
-                      ),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ],
                     ),
                   ),
                 ),
+              ),
 
-                Positioned(
-                  top: 340,
-                  right: -55,
-                  child: Transform(
-                    transform: Matrix4.skew(-3, 3),
-                    child: Padding(
-                      padding: const EdgeInsets.all(0),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: mySecondActors.map((path) {
-                              return Padding(
-                                padding: const EdgeInsets.all(12),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Image(
-                                    image: AssetImage(path),
-                                  ),
+              Positioned(
+                top: 340,
+                right: -55,
+                child: Transform(
+                  transform: Matrix4.skew(-3, 3),
+                  child: Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: mySecondActors.map((path) {
+                            return Padding(
+                              padding: const EdgeInsets.all(12),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image(
+                                  image: AssetImage(path),
                                 ),
-                              );
-                            }).toList(),
-                          ),
-                        ],
-                      ),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
           // headline section
           const Positioned(
-            bottom: 350,
+            bottom: 230,
             left: 0,
             right: 0,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'For actors, directors, and ',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'everyone in-between',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'For actors, directors, and ',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'everyone in-between',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
           ),
 
           // start explore
           const Positioned(
-            bottom: 280,
+            bottom: 180,
             left: 0,
             right: 0,
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 1,
-                    width: 100,
-                    child: Divider(
-                      color: Colors.black,
-                      thickness: 0.2,
-                    ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 1,
+                  width: 100,
+                  child: Divider(
+                    color: Colors.black,
+                    thickness: 0.2,
                   ),
-                  SizedBox(width: 20),
-                  Text(
-                    'Start exploring',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w200,
-                    ),
+                ),
+                SizedBox(width: 20),
+                Text(
+                  'Start exploring',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w200,
                   ),
-                  SizedBox(width: 20),
-                  SizedBox(
-                    height: 1,
-                    width: 100,
-                    child: Divider(
-                      color: Colors.black,
-                      thickness: 0.2,
-                    ),
+                ),
+                SizedBox(width: 20),
+                SizedBox(
+                  height: 1,
+                  width: 100,
+                  child: Divider(
+                    color: Colors.black,
+                    thickness: 0.2,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
 
           // buttons section
           Positioned(
-            bottom: 100,
+            bottom: 20,
             left: 0,
             right: 0,
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'mainscreen');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0,
-                      backgroundColor: Colors.black,
-                      fixedSize: const Size(double.infinity, 60),
-                      minimumSize: const Size(200, 60),
-                      maximumSize: const Size(370, 60),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    child: const Text(
-                      'Explore as an actor',
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'mainscreen');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    backgroundColor: Colors.black,
+                    fixedSize: const Size(double.infinity, 60),
+                    minimumSize: const Size(200, 60),
+                    maximumSize: const Size(370, 60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                  const SizedBox(
-                    height: 20,
+                  child: const Text(
+                    'Explore as an actor',
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      elevation: 0,
-                      backgroundColor: Colors.white,
-                      fixedSize: const Size(double.infinity, 60),
-                      minimumSize: const Size(200, 60),
-                      maximumSize: const Size(370, 60),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      side: const BorderSide(
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    backgroundColor: Colors.white,
+                    fixedSize: const Size(double.infinity, 60),
+                    minimumSize: const Size(200, 60),
+                    maximumSize: const Size(370, 60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    side: const BorderSide(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                  child: const Text(
+                    'Explore as a producer',
+                    style: TextStyle(
+                        fontSize: 16,
                         color: Colors.black,
-                        width: 1,
-                      ),
-                    ),
-                    child: const Text(
-                      'Explore as a producer',
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w300),
-                    ),
-                  )
-                ],
-              ),
+                        fontWeight: FontWeight.w300),
+                  ),
+                )
+              ],
             ),
           )
         ],
