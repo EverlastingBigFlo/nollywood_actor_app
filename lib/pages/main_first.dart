5aca9cdd0bc242e0d39fb4003d3846824faa8630
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-14 12:59:48 +0000

    14 Mar 2024, 05:59

--- lib/pages/main_first.dart
+++ lib/pages/main_first.dart
@@ -13,35 +13,38 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            alignment: const Alignment(-1, 1),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                // username and caption
-                Text(
-                  '@AdesuaEtomi',
-                  style:
-                      TextStyle(fontSize: 14.63, fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'The Seagull (Anton Chekhov)',
-                  style:
-                      TextStyle(fontSize: 13.59, fontWeight: FontWeight.w300),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  '#hollywood-english',
-                  style:
-                      TextStyle(fontSize: 13.593, fontWeight: FontWeight.w400),
-                ),
-              ],
+          // username and caption
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              alignment: const Alignment(-1, 1),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    '@AdesuaEtomi',
+                    style:
+                        TextStyle(fontSize: 14.63, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'The Seagull (Anton Chekhov)',
+                    style:
+                        TextStyle(fontSize: 13.59, fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '#hollywood-english',
+                    style: TextStyle(
+                        fontSize: 13.593, fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
             ),
           )
         ],
