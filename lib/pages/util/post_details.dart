1410d5ccea56c57ca67470dbe84f06db26fe1c95
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-14 15:50:39 +0000

    14 Mar 2024, 08:50

--- lib/pages/util/post_details.dart
+++ lib/pages/util/post_details.dart
@@ -27,74 +27,76 @@ class _PostDetailsState extends State<PostDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            alignment: const Alignment(0, 0),
-            child: Image(
-              image: AssetImage(widget.userPost),
-            ),
-          ),
-          // username and caption
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              alignment: const Alignment(-1, 1),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    '@${widget.username}',
-                    style: const TextStyle(
-                        fontSize: 14.63, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    widget.songTitle,
-                    style: const TextStyle(
-                        fontSize: 13.59, fontWeight: FontWeight.w300),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    widget.description,
-                    style: const TextStyle(
-                        fontSize: 13.593, fontWeight: FontWeight.w400),
-                  ),
-                ],
-              ),
-            ),
-          ),
+      body: Text('hellio'),
+      // Stack(
+      //   children: [
+      //     Container(
+      //       alignment: const Alignment(0, 0),
+      //       child: Image(
+      //         image: AssetImage(widget.userPost),
+      //       ),
+      //     ),
+      //     // username and caption
+      //     Padding(
+      //       padding: const EdgeInsets.all(20.0),
+      //       child: Container(
+      //         alignment: const Alignment(-1, 1),
+      //         child: Column(
+      //           crossAxisAlignment: CrossAxisAlignment.start,
+      //           mainAxisAlignment: MainAxisAlignment.end,
+      //           children: [
+      //             Text(
+      //               '@${widget.username}',
+      //               style: const TextStyle(
+      //                   fontSize: 14.63, fontWeight: FontWeight.w500),
+      //             ),
+      //             const SizedBox(
+      //               height: 20,
+      //             ),
+      //             Text(
+      //               widget.songTitle,
+      //               style: const TextStyle(
+      //                   fontSize: 13.59, fontWeight: FontWeight.w300),
+      //             ),
+      //             const SizedBox(
+      //               height: 5,
+      //             ),
+      //             Text(
+      //               widget.description,
+      //               style: const TextStyle(
+      //                   fontSize: 13.593, fontWeight: FontWeight.w400),
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ),
 
-          //Buttons
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              alignment: const Alignment(1, 1),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  MyButton(icon: Icons.favorite, number: widget.numbersOfLikes),
-                  MyButton(
-                      icon: Icons.chat,
-                      number:
-                          // '25'
-                          widget.numbersOfComment),
-                  MyButton(
-                      icon: Icons.share,
-                      number:
-                          // '12'
-                          widget.numbersOfShare),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+      //     //Buttons
+      //     Padding(
+      //       padding: const EdgeInsets.all(20.0),
+      //       child: Container(
+      //         alignment: const Alignment(1, 1),
+      //         child: Column(
+      //           mainAxisAlignment: MainAxisAlignment.end,
+      //           children: [
+      //             MyButton(icon: Icons.favorite, number: widget.numbersOfLikes),
+      //             MyButton(
+      //                 icon: Icons.chat,
+      //                 number:
+      //                     // '25'
+      //                     widget.numbersOfComment),
+      //             MyButton(
+      //                 icon: Icons.share,
+      //                 number:
+      //                     // '12'
+      //                     widget.numbersOfShare),
+      //           ],
+      //         ),
+      //       ),
+      //     )
+      //   ],
+      // ),
+
       bottomNavigationBar: BottomNavigationBar(
           items: const [BottomNavigationBarItem(icon: Icon(Icons.house))]),
     );
