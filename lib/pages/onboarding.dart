8556144bd9ff7e6572d3206bd716c7cfc6cf9d87
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-15 17:06:42 +0000

    15 Mar 2024, 10:06

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -47,21 +47,23 @@ class _OnboardingState extends State<Onboarding> {
               // Actors section
               Positioned(
                 top: 200,
-                right: -45,
+                right: -65,
                 child: Transform(
                   transform: Matrix4.skew(-3, 3),
                   child: Padding(
-                    padding: const EdgeInsets.all(0),
+                    padding: const EdgeInsets.only(bottom: 10),
                     child: Column(
                       children: [
                         Row(
                           children: myFirstActors.map((path) {
                             return Padding(
-                              padding: const EdgeInsets.all(12),
+                              padding: const EdgeInsets.all(7),
                               child: ClipRRect(
                                 borderRadius: BorderRadius.circular(10),
                                 child: Image(
                                   image: AssetImage(path),
+                                  height: 135,
+                                  fit: BoxFit.cover,
                                 ),
                               ),
                             );
@@ -75,23 +77,23 @@ class _OnboardingState extends State<Onboarding> {
 
               Positioned(
                 top: 340,
-                right: -67,
+                right: -70,
                 child: Transform(
                   transform: Matrix4.skew(-3, 3),
                   child: Padding(
-                    padding: const EdgeInsets.all(0),
+                    padding: const EdgeInsets.only(top: 20),
                     child: Column(
                       children: [
                         Row(
                           children: mySecondActors.map((path) {
                             return Padding(
-                              padding: const EdgeInsets.all(8),
+                              padding: const EdgeInsets.all(7),
                               child: ClipRRect(
-                                borderRadius: BorderRadius.circular(6),
+                                borderRadius: BorderRadius.circular(10),
                                 child: Image(
                                   image: AssetImage(path),
                                   fit: BoxFit.contain,
-                                  height: 130,
+                                  height: 135,
                                 ),
                               ),
                             );
