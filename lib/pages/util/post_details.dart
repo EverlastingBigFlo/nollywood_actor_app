922be2b6e7c58776fd68614b04fad0638232f4be
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-15 10:18:06 +0000

    15 Mar 2024, 03:18

--- lib/pages/util/post_details.dart
+++ lib/pages/util/post_details.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:nollywood_actor_app/pages/util/button.dart';
 
 class PostDetails extends StatefulWidget {
@@ -168,22 +169,7 @@ class _PostDetailsState extends State<PostDetails> {
             ),
           ),
 
-          // progress bar section
-          FractionallySizedBox(
-            alignment: Alignment.bottomCenter,
-            widthFactor: 1.0,
-            child: SizedBox(
-              height: 3.0,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: Colors.grey[400],
-                  valueColor: const AlwaysStoppedAnimation(Colors.white),
-                ),
-              ),
-            ),
-          )
+        
         ],
       ),
       // bottom navigation
